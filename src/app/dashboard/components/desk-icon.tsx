fe244e63a8f0746c26a0ffa19b06cbcd4eab4c4e
Domenico Rossini <domenico.dex@gmail.com>
2026-01-09 15:12:58 +0000

    okay lavora meglio sull'icona delle scrivanie, non mi semrano scrivanie

--- src/app/dashboard/components/desk-icon.tsx
+++ src/app/dashboard/components/desk-icon.tsx
@@ -4,6 +4,8 @@ export function DeskIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -12,10 +14,11 @@ export function DeskIcon(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M2 6h20" />
-      <path d="M4 6v12" />
-      <path d="M20 6v12" />
-      <path d="M15 22H9" />
+      <path d="M22 13V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v6" />
+      <path d="M2 13h20" />
+      <path d="M12 21v-8" />
+      <path d="M7 21v-8" />
+      <path d="M17 21v-8" />
     </svg>
   );
 }
